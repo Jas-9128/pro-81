f8f34087876380b107600fb2d044a76528bf2187
Jas-9128 <83761825+Jas-9128@users.noreply.github.com>
2021-10-05 16:56:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+canvas=document.getElementById("mycanvas");
+ctx=canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle= "grey";
+ctx.lineWidth=2;
+ctx.rect(150,143,430,200);
+ctx.strokeStyle();
+
+
+
+ctx.beginPath();
+ctx.strokeStyle="blue";
+ctx.lineWidth=2;
+ctx.arc(210,40,0,2*Math.PI);
+ctx.strokeStyle();
+
+ctx.beginPath();
+ctx.strokeStyle="black";
+ctx.lineWidth=2;
+ctx.arc(330,210,40,0,2*Math.PI);
+ctx.strokeStyle();
+
